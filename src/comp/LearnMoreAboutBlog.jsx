71f8de4e1170e4bc897f0985a6b79f54df1ddeb6
Mohammad Ghous Mujtaba <mohammad.ghous.mujtabaa@gmail.com>
2024-11-14 01:55:55 +0530

    new

--- src/comp/LearnMoreAboutBlog.jsx
+++ src/comp/LearnMoreAboutBlog.jsx
@@ -8,7 +8,7 @@ const LearnMoreAboutBlog = () => {
       <img src={Blog1} alt="Blog" className="rounded-md mb-4" />
       <p>
         This page includes more detailed information about our blogs and topics of interest. 
-        Add as much content as you like here.
+        Add as much content as you like here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium quas maxime, ipsam ipsa et tempora expedita dolores dolor cum quo ratione dolorem omnis ad, facere laudantium maiores eum fugit optio! Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id quasi, ad architecto ab, voluptatem laborum, exercitationem provident commodi optio accusantium nemo reiciendis. Et porro ducimus suscipit cum, nostrum cupiditate dicta!.
       </p>
     </div>
   );
